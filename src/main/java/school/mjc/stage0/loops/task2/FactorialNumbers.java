35511dfe446653e434fdbd7d6d87b51fbea79cb4
martam00 <marta.michniacka1@gmail.com>
2023-07-23 14:44:29 +0200

    while loops

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int multiply = 1;
+        int result = 1;
+        System.out.println(result);
+        while (multiply <= printToInclusive) {
+            result *= multiply;
+            System.out.println(result);
+            multiply++;
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int multiply = 0;
+        int counter = 0;
+        if (multiplyByAndToInclusive == 0) {
+            return;
+        }
+        int temp = multiplyByAndToInclusive;
+        if (multiplyByAndToInclusive < 0) {
+            temp = multiplyByAndToInclusive * -1;
+        }
+        while (counter <= temp) {
+            System.out.println(multiply);
+            counter++;
+            multiply = counter * multiplyByAndToInclusive;
+        }
     }
 }
